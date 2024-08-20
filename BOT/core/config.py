4efd166a1a5e72d1adf1068a91786ee66935a80a
ilyab <i-baykov@internet.ru>
2024-08-20 23:35:57 +0300

    Стартовая настройка приложения

---
+++ BOT/core/config.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 15
+    max_overflow: int = 10
+
+
+class MainSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__"
+    )
+    db: DatabaseConfig
+
+
+settings = MainSettings()
---
+++ BOT/core/models/db_helper.py
@@ -0,0 +1,36 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from BOT.core.config import settings
+
+
+class DatabaseHelper:
+    def __init__(self,
+                 url: str,
+                 echo: bool = False,
+                 echo_pool: bool = False,
+                 pool_size: int = 5,
+                 max_overflow: int = 10
+                 ):
+        self.async_engine = create_async_engine(url=url, echo=echo, echo_pool=echo_pool,
+                                                pool_size=pool_size, max_overflow=max_overflow)
+
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(bind=self.async_engine,
+                                                                                    autoflush=False, autocommit=False,
+                                                                                    expire_on_commit=False)
+
+    async def dispose(self):
+        await self.async_engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow
+)
